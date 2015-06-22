8ac587917d86db0126b372a181b354af624db226
Kevin Armstrong <kevin2.armstrong@capitalone.com>
2015-06-22 13:23:53 -0400

    Adding Google Maps iOS sample app

--- GMapsDemo/ViewController.swift
+++ GMapsDemo/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var viewMap: GMSMapView!
+    
+    @IBOutlet weak var bbFindAddress: UIBarButtonItem!
+    
+    @IBOutlet weak var lblInfo: UILabel!
+    
+    var locationManager = CLLocationManager()
+    
+    var didFindMyLocation = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        
+        let camera: GMSCameraPosition = GMSCameraPosition.cameraWithLatitude(48.857165, longitude: 2.354613,zoom: 8.0)
+        
+        viewMap.addObserver(self, forKeyPath: "myLocation", options: NSKeyValueObservingOptions.New, context: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +38,68 @@ class ViewController: UIViewController {
     }
 
 
+    // MARK: IBAction method implementation
+    
+    @IBAction func changeMapType(sender: AnyObject) {
+        let actionSheet = UIAlertController(title: "Map Types", message: "Select map type:", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+        let normalMapTypeAction = UIAlertAction(title: "Normal", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            self.viewMap.mapType = kGMSTypeNormal
+        }
+        
+        let terrainMapTypeAction = UIAlertAction(title: "Terrain", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            self.viewMap.mapType = kGMSTypeTerrain
+        }
+        
+        let hybridMapTypeAction = UIAlertAction(title: "Hybrid", style: UIAlertActionStyle.Default) { (alertAction) -> Void in
+            self.viewMap.mapType = kGMSTypeHybrid
+        }
+        
+        let cancelAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.Cancel) { (alertAction) -> Void in
+            
+        }
+        
+        actionSheet.addAction(normalMapTypeAction)
+        actionSheet.addAction(terrainMapTypeAction)
+        actionSheet.addAction(hybridMapTypeAction)
+        actionSheet.addAction(cancelAction)
+        
+        presentViewController(actionSheet, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func findAddress(sender: AnyObject) {
+    
+    }
+    
+    
+    @IBAction func createRoute(sender: AnyObject) {
+    
+    }
+    
+    
+    @IBAction func changeTravelMode(sender: AnyObject) {
+    
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.AuthorizedWhenInUse {
+            viewMap.myLocationEnabled = true
+        }
+    }
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if !didFindMyLocation {
+            let myLocation: CLLocation = change[NSKeyValueChangeNewKey] as! CLLocation
+            viewMap.camera = GMSCameraPosition.cameraWithTarget(myLocation.coordinate, zoom: 10.0)
+            viewMap.settings.myLocationButton = true
+            
+            didFindMyLocation = true
+        }
+    }
+    
+    
+    
+    
 }
 
